7d403f8e0b0e9482b686a5c8cd8a1cf197e9d6fe
Sudarshan <sudarb11@gmail.com>
2016-01-04 18:18:14 +0530

    boolean value validations

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,4 +1,5 @@
 class Feedback < ActiveRecord::Base
 
-  validates :meeting_with, :meeting_happened, :was_useful, :presence => true
+  validates :meeting_with, :presence => true
+  validates_inclusion_of :meeting_happened, :was_useful, in: [true, false]
 end
